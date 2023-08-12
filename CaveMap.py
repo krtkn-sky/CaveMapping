9bf1a0bdbfe1e1362993cec0162420337dae187e
AakashK2003 <aakashkarthikeyan3@gmail.com>
2023-08-12 23:23:21 +0530

    Intial commit

--- CaveMap.py
+++ CaveMap.py
@@ -1,9 +1,14 @@
 import folium 
 import pandas
 
-data = pandas.read_csv("Caves.txt")
-lat = list(data["Latitude"])
-lon = list(data["Longitude"])
+data = pandas.read_csv("/home/akn29/Desktop/CaveMapping/Caves.csv")
+lat = list(data[" Latitude"])
+lon = list(data[" Longitude"])
 
-print(lat)
-print(lon)
+
+map = folium.Map(location=[23.542137394,78.295198388],
+                 zoom_start=6,
+                 tiles="Stamen Terrain")
+
+#print(lon)
+#print(lat)
