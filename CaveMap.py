d5fa76a2264e475aa19eeb3d5bdbe3d084caa482
AakashK2003 <aakashkarthikeyan3@gmail.com>
2023-08-12 23:33:25 +0530

    Test

--- CaveMap.py
+++ CaveMap.py
@@ -4,12 +4,20 @@ import pandas
 data = pandas.read_csv("/home/akn29/Desktop/CaveMapping/Caves.csv")
 lat = list(data[" Latitude"])
 lon = list(data[" Longitude"])
-
+names = list(data[" Cave Name"])
 
 map = folium.Map(location=[23.542137394,78.295198388],
                  zoom_start=6,
                  tiles="Stamen Terrain")
 
+fg = folium.FeatureGroup(name="My Map")
+
+for lt,ln,nm in zip(lat,lon,names):
+    fg.add_child(folium.Marker(location=[lt,ln],
+                               popup=str(nm),
+                               icon=folium.Icon(color="red")))
+    
+map.add_child(fg)
 map.save("Caves.html")
 
 #print(lon)
