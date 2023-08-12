1e91523c778a7ecbee6e9e43fe38c650899e3fb1
AakashK2003 <aakashkarthikeyan3@gmail.com>
2023-08-13 00:41:31 +0530

    colored markers

--- CaveMap.py
+++ CaveMap.py
@@ -7,6 +7,16 @@ lon = list(data[" Longitude"])
 names = list(data[" Cave Name"])
 elev = list(data[" Elevation (m)"])
 
+def colorGen(elevation):
+    if elevation < 100:
+        return 'blue'
+    elif 100 <= elevation < 500:
+        return 'green'
+    elif 500 <= elevation < 1000:
+        return 'darkgreen'
+    else:
+        return "black"
+
 map = folium.Map(location=[23.542137394,78.295198388],
                  zoom_start=4,
                  tiles="Stamen Terrain")
@@ -16,7 +26,7 @@ fg = folium.FeatureGroup(name="My Map")
 for lt,ln,nm,el in zip(lat,lon,names,elev):
     fg.add_child(folium.Marker(location=[lt,ln],
                                popup=str(nm)+" "+str(el)+"m",
-                               icon=folium.Icon(color="red")))
+                               icon=folium.Icon(color=colorGen(el))))
     
 map.add_child(fg)
 map.save("Caves.html")
