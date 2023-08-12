f9432bc349710cd73a8755d88e30bae975a1eae4
AakashK2003 <aakashkarthikeyan3@gmail.com>
2023-08-12 23:26:46 +0530

    Map

--- CaveMap.py
+++ CaveMap.py
@@ -10,5 +10,7 @@ map = folium.Map(location=[23.542137394,78.295198388],
                  zoom_start=6,
                  tiles="Stamen Terrain")
 
+map.save("Caves.html")
+
 #print(lon)
 #print(lat)
